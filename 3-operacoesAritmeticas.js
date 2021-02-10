38de7175b29dc9aee1c106b9711b80fce3302185
cortelucas <corte1994.lc@gmail.com>
2021-02-10 09:19:16 -0300

    - aritmetica

---
+++ 3-operacoesAritmeticas.js
@@ -0,0 +1,18 @@
+console.log('--- Operações Aritméticas ---');
+
+console.log('')
+console.log('Operações com 1 e 2');
+console.log(`Adição ${1 + 2}`);
+console.log(`Subtração ${1 - 2}`);
+console.log(`Multiplicação ${1 * 2}`);
+console.log(`DIvisão ${1 / 2}`);
+
+//Ordem de precedencia
+
+console.log(`
+    -- Ordem de Precedênca --
+    
+    10 + 8 * 2 é diferente de (10 + 8) * 2
+    10 + 8 * 2 = ${10 + 8 * 2}
+    (10 + 8) * 2 = ${(10 + 8) * 2}
+`);
