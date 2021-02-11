fe74c9d72be3c2b6684d0983e6b938049ca54d2d
cortelucas <corte1994.lc@gmail.com>
2021-02-11 09:37:09 -0300

    melhoria da explicação

--- 2-trabalhandoComVariaveis.js
+++ 2-trabalhandoComVariaveis.js
@@ -1,8 +1,28 @@
-console.log('Trabalhando com variáveis.');
+console.log('--- Trabalhando com variáveis. ---');
 
-const lucas = {
+const nome = 'Lucas Corte'
+const idade = 27;
+const cpf = 41193772885;
+
+console.log(`
+Aqui trabalhamos guardando 3 valores em 3 variáveis.
+e podemos concatená-la da seguinte maneira.
+
+Eu ${nome}, inscrito sob o CPF ${cpf}, declaro que tenho a idade de ${idade} anos.
+`)
+
+const pessoa = {
     nome: 'Lucas Corte',
+    cpf: 41193772885,
     idade: 27
 }
+console.log(typeof(pessoa))
 
-console.log(`O seu nome é ${lucas.nome} e sua idade é ${lucas.idade}!`);
+console.log(`
+Mas também podemos declarar em uma unica variavel, no qual chamamos de Objeto.
+Aqui declarei o objeto pessoa que recebe nela todas as informações.
+Para acessarmos as mesmas, devemos colocar o nome da variável, seguida de um ponto.
+Demonstro abaixo:
+
+Eu ${pessoa.nome}, inscrito sob o CPF ${pessoa.cpf}, declaro que tenho a idade de ${pessoa.idade} anos.
+`);
