e89fd590d699e4a3fe64dca981cca1e01d486805
cortelucas <corte1994.lc@gmail.com>
2021-02-11 09:29:13 -0300

    - melhoramento da explicação

--- 1-meuPrimeiroPrograma.js
+++ 1-meuPrimeiroPrograma.js
@@ -1,9 +1,16 @@
-console.log('Meu primeiro programa: Trabalhando com variáveis');
+console.log('--- Meu primeiro programa: Trabalhando com variáveis ---');
 
 const idade = 26;
 
-console.log(idade);
-console.log(idade + 2);
+console.log(`
+Aqui imprimimos a variável idade com o valor de ${idade}.
+Com as variáveis podemos trabalhar com elas, por exemplo: 
+idade + 2 = ${idade + 2}. Incrementando 2 a variável.
+`)
 
 const idadeSomada = idade + 2;
-console.log(idadeSomada);
+console.log(`
+Podemos tbm guardar uma variável em outra variável.
+Declaramos a variável idadeSomada, incrementando 2 a ela.
+Assim fica idadeSomada = ${idadeSomada}.
+`);
