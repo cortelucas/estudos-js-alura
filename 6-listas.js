0ec7baf2d2eb0961f4d2f74801206f46f169d07e
cortelucas <corte1994.lc@gmail.com>
2021-02-11 10:49:36 -0300

    atualização

--- 6-listas.js
+++ 6-listas.js
@@ -10,11 +10,28 @@ console.log(`
 Quando temos varias variaveis que serão umas especie de lista, podemos fazer uma.
 no lugar de declararmos, let Salvador = 'Salvador', let RioDeJaneiro = 'Rio de Janeiro'...
 podemos declarar como um new Array ou um const lista = [];
-`)
 
+para colocarmos novos dados dentro da lista usamos o push
+`);
 listaDeDestinos.push(`Curitiba`);
-
 console.log(`
 Destinos possíveis:
 ${listaDeDestinos}
+`);
+
+console.log(`Agora, para tirarmos algo, temos que usar o splice(), onde temos que passar o local que esta.
+por exemplo, deletarei São Paulo, que esta na segunda posição;
+listaDeDestinos.splice(1, 1);
+Usamos este exemplo acima.
+`);
+listaDeDestinos.splice(1, 1);
+console.log(`
+Destinos possíveis:
+${listaDeDestinos}
+`);
+
+console.log(`
+Usamos para acessar a posição utilizando [i], por exemplo:
+listaDeDestinos[1]
+${listaDeDestinos[1]}
 `)
