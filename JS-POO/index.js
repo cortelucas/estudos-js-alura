42e2b422762c546fea632a7d49834a11ad696d24
cortelucas <corte1994.lc@gmail.com>
2021-02-12 08:45:23 -0300

    criação do metodo sacar

--- JS-POO/index.js
+++ JS-POO/index.js
@@ -2,24 +2,33 @@ class Cliente {
     nome;
     CPF;
     rg;
+}
+
+class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+        }
+    };
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = `Lucas`;
 cliente1.CPF = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 cliente1.rg = 1122233345;
 
+
+const cliente2 = new Cliente();
 cliente2.nome = `Andressa`;
 cliente2.CPF = 88899977709;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 cliente2.rg = 3322211101;
+
+const contaCorrenteCliente1 = new ContaCorrente();
+contaCorrenteCliente1.saldo = 0;
+contaCorrenteCliente1.agencia = 1001;
 
 console.log(cliente1);
 console.log(cliente2);
